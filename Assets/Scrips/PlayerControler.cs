28d4c2060c329de6aaa4ae931a3a39d84f1b6ad8
Mathijs Lemmers <mathijs750@gmail.com>
2016-10-01 19:52:25 +0200

    Add project, scaling window and a player stub

---
+++ Assets/Scrips/PlayerControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField,Range(0,1)]
+    private float stepSize = 0.25f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0), stepSize));
+    }
+}
