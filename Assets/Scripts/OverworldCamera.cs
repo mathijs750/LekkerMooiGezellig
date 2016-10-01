e4e9424329be17bd986d7ad143053247c783213e
Mathijs Lemmers <mathijs750@gmail.com>
2016-10-01 20:47:20 +0200

    Fix typo in folder name

---
+++ Assets/Scripts/OverworldCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Transform player;
+
+	// Use this for initialization
+	void Awake () {
+	   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
