f459f33a0a76ed75b1519bb4b31fea51d2e9913b
Mathijs Lemmers <mathijs750@gmail.com>
2016-10-02 00:00:02 +0200

    Tweak camera movement to be inline with gameboy's limitations

--- Assets/Scripts/OverworldCamera.cs
+++ Assets/Scripts/OverworldCamera.cs
@@ -1,32 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OverworldCamera : MonoBehaviour {
-    
+public class OverworldCamera : MonoBehaviour
+{
+
     [SerializeField]
     private Transform player;
-    [SerializeField,Range(0,5)]
+    [SerializeField, Range(0, 5)]
     private float lerpSpringiness = 3f;
-    private Transform centerPoint;
-    private Vector3 oldPos;
+    [SerializeField]
+    private float deadZone;
 
-	// Use this for initialization
-	void Awake ()
+    private Transform centerPoint;
+
+    private Vector3 oldPos, newPos;
+
+    // Use this for initialization
+    void Awake()
     {
         centerPoint = transform.GetChild(0); // Make sure i't the first child
-        oldPos = player.position + centerPoint.localPosition;
-        transform.position = oldPos;
+        newPos = player.position - centerPoint.localPosition - new Vector3(-.5f, .5f);
+        transform.position = newPos;
+        oldPos = newPos;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         oldPos = transform.position;
 
         if (StateMachine.CurrentPlayState == PlayState.OverWorld)
         {
-            transform.position = Vector3.Lerp(oldPos, player.position - centerPoint.localPosition, Time.deltaTime * lerpSpringiness);
-
+            if (Mathf.Abs(player.position.x - centerPoint.position.x) > deadZone / 2 || Mathf.Abs(player.position.y - centerPoint.position.y) > deadZone / 2)
+            {
+                newPos = player.position - centerPoint.localPosition;
+                transform.position = Vector3.Lerp(oldPos, newPos, Time.deltaTime * lerpSpringiness);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(oldPos, newPos, Time.deltaTime * (lerpSpringiness / 2));
+            }
         }
     }
+    /*
+    public void OnDrawGizmos()
+    {
+        if (centerPoint.position == null) { return; }
+        Gizmos.DrawCube(centerPoint.position, new Vector3(deadZone, deadZone, deadZone));
+    }*/
 }
