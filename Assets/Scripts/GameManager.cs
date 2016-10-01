1191380c3a093f44ce4e5c739d60f049e3c12a47
Mathijs Lemmers <mathijs750@gmail.com>
2016-10-02 00:00:14 +0200

    Add fake dialogue UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject dialogueUI;
+
+    public static void activateDialogue(GameObject npc)
+    {
+        StateMachine.setPlayState(PlayState.Dialogue);     
+    }
+
+
     void Awake()
     {
+        // Application.targetFrameRate = 59; // 59.73fps
         StateMachine.onGameStateChanged += onGameStateChange;
+        StateMachine.onPlayStateChanged += onPlayStateChange;
         StateMachine.setGameState(GameState.MainMenu); // for debugging
 
         StateMachine.setGameState(GameState.Playing); // for debugging
@@ -15,23 +26,24 @@ public class GameManager : MonoBehaviour
     void OnDisable()
     {
         StateMachine.onGameStateChanged -= onGameStateChange;
+        StateMachine.onPlayStateChanged -= onPlayStateChange;
     }
 
-    void Start()
-    {
-    }
-
-
-    void Update()
-    {
-
-    }
 
     private void onGameStateChange(GameState prevState, GameState curState)
     {
         if (prevState == GameState.MainMenu && curState == GameState.Playing)
         {
             Debug.Log("The Game is started");
+        }
+    }
+
+    private void onPlayStateChange(PlayState prevState, PlayState curState)
+    {
+        if (prevState == PlayState.OverWorld && curState == PlayState.Dialogue)
+        {
+            if (dialogueUI != null) { dialogueUI.SetActive(true); }
+            Debug.Log("BABBEL TIJD!!!!!");
         }
     }
 }
