6513be4a351201913618b1afba6863841fecb16f
Mathijs Lemmers <mathijs750@gmail.com>
2016-10-03 03:15:44 +0200

    Tweak walking again

--- Assets/Scripts/OverworldCamera.cs
+++ Assets/Scripts/OverworldCamera.cs
@@ -3,49 +3,30 @@ using System.Collections;
 
 public class OverworldCamera : MonoBehaviour
 {
-
     [SerializeField]
     private Transform player;
-    [SerializeField, Range(0, 5)]
-    private float lerpSpringiness = 3f;
-    [SerializeField]
-    private float deadZone;
+    private Transform centerPoint, cornerPoint;
 
-    private Transform centerPoint;
+    public Vector3 topCorner
+    {
+        get
+        {
+            if (cornerPoint != null) { return cornerPoint.position; }
+            else { return Vector3.zero; }
+        }
+    }
 
-    private Vector3 oldPos, newPos;
-
-    // Use this for initialization
     void Awake()
     {
         centerPoint = transform.GetChild(0); // Make sure i't the first child
-        newPos = player.position - centerPoint.localPosition - new Vector3(-.5f, .5f);
-        transform.position = newPos;
-        oldPos = newPos;
+        cornerPoint = transform.GetChild(1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        oldPos = transform.position;
-
         if (StateMachine.CurrentPlayState == PlayState.OverWorld)
         {
-            if (Mathf.Abs(player.position.x - centerPoint.position.x) > deadZone / 2 || Mathf.Abs(player.position.y - centerPoint.position.y) > deadZone / 2)
-            {
-                newPos = player.position - centerPoint.localPosition;
-                transform.position = Vector3.Lerp(oldPos, newPos, Time.deltaTime * lerpSpringiness);
-            }
-            else
-            {
-                transform.position = Vector3.Lerp(oldPos, newPos, Time.deltaTime * (lerpSpringiness / 2));
-            }
+            transform.position = player.position - centerPoint.localPosition;
         }
     }
-    /*
-    public void OnDrawGizmos()
-    {
-        if (centerPoint.position == null) { return; }
-        Gizmos.DrawCube(centerPoint.position, new Vector3(deadZone, deadZone, deadZone));
-    }*/
 }
--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,36 +7,58 @@ public class PlayerControler : MonoBehaviour
     [SerializeField]
     private float StepTreshold = 1f;
     [SerializeField]
-    private float stepSize = 1f;
+    private float stepSpeed = 1f;
+    [SerializeField]
+    private float runModifier = 1f;
 
     private SpriteControler spriteCon;
-    private Vector2 newPos;
-    private Vector2 lastLookDirection;
+    private Vector3 newPosition, lastLookDirection;
     /// Left w, Down x, Right y, Up z
     private Vector4 times = Vector4.zero;
     private bool isMoving;
-    private int stepTimer;
+
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (StateMachine.CurrentGameState == GameState.Playing && StateMachine.CurrentPlayState == PlayState.OverWorld)  //NPC = layer 9
         {
-            Vector2 input = movementInput();// * stepSize;
-            if (isNotBlocked(input)) { newPos += input; }
+            // Get a unit vector based on key input and save it for later use
+            Vector3 inputDirection = dpadInput();
+            if (inputDirection.magnitude > 0.1f)
+            {
+                lastLookDirection = inputDirection;
+            }
+            if (isFreeToMove(inputDirection))
+            {
+                if (Input.GetButton("B")) { newPosition += inputDirection * runModifier; }
+                else { newPosition += inputDirection; }
+            }
 
-            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime + .1f);
-            AlignPosToGrid();
-
-            if (input.magnitude > 0.1f) { lastLookDirection = input; }
+            // Check is the lerp is close to 0 to align to the grid
+            float dist = Mathf.Abs(Vector3.Distance(transform.position, newPosition));
+            if (dist <= 0.1)
+            {
+                isMoving = false;
+                transform.position = newPosition;
+            }
+            else
+            {
+                isMoving = true;
+                if (Input.GetButton("B"))
+                {
+                    transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * stepSpeed);
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * stepSpeed);
+                }
+            }
 
 
             if (Input.GetButtonDown("A"))
             {
                 RaycastHit2D hit = Physics2D.Raycast(transform.position, lastLookDirection, 2, 1 << 9);
-                //Debug.DrawLine(transform.position, transform.position + new Vector3(lastLookDirection.x, lastLookDirection.y), Color.green,2);
                 if (hit)
                 {
                     if (hit.collider.tag == "CanSpeak")
@@ -48,25 +70,15 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
-    private void AlignPosToGrid()
-    {
-        if (Mathf.Floor(Vector3.Magnitude(transform.position - new Vector3(newPos.x, newPos.y, 0)) * 1000) > 0) { isMoving = true; }
-        else
-        {
-            isMoving = false;
-            transform.position = newPos;
-        }
-    }
 
     void OnEnable()
     {
-        newPos = transform.position;
         spriteCon = transform.GetChild(0).GetComponent<SpriteControler>();
+        newPosition = transform.position;
         isMoving = false;
-        stepTimer = 0;
     }
 
-    private bool isNotBlocked(Vector2 input)
+    private bool isFreeToMove(Vector2 input)
     {
         // the "most" best way
         RaycastHit2D hitA = Physics2D.Raycast(transform.position + new Vector3(-0.5f, 0.5f, 0), input, 1, 1 << 9);
@@ -84,50 +96,42 @@ public class PlayerControler : MonoBehaviour
         }
     }
 
-    private Vector2 movementInput()
+    private Vector2 dpadInput()
     {
-        Vector2 pos = Vector2.zero;
-
         // Button ups
         //////////////
 
-        if (Input.GetButtonUp("Left"))
+        if (Input.GetButtonUp("Left") && !isMoving)
         {
-            if (times.w > 0 && !isMoving)
+            if (times.w > 0.1f)
             {
                 spriteCon.Direction = spriteDirection.left;
             }
             times.w = 0;
         }
-
-        else if (Input.GetButtonUp("Right"))
+        else if (Input.GetButtonUp("Right") && !isMoving)
         {
-            if (times.y > 0 && !isMoving)
+            if (times.y > 0.1f)
             {
                 spriteCon.Direction = spriteDirection.right;
             }
             times.y = 0;
-
         }
-
-        if (Input.GetButtonUp("Up"))
+        else if (Input.GetButtonUp("Up") && !isMoving)
         {
-            if (times.z > 0 && !isMoving)
+            if (times.z > 0.1f)
             {
                 spriteCon.Direction = spriteDirection.up;
             }
             times.z = 0;
-
         }
-
-        else if (Input.GetButtonUp("Down"))
+        else if (Input.GetButtonUp("Down") && !isMoving)
         {
-            if (times.x > 0 && !isMoving)
+            if (times.x > 0.1f)
             {
                 spriteCon.Direction = spriteDirection.down;
             }
             times.x = 0;
-
         }
 
         // Button presses
@@ -135,50 +139,46 @@ public class PlayerControler : MonoBehaviour
 
         if (Input.GetButton("Left"))
         {
-            times.w++;
             if (times.w > StepTreshold)
             {
-                isMoving = true;
                 spriteCon.Direction = spriteDirection.left;
-                pos += Vector2.left;
-                times.w = 0;
+                times = Vector4.zero;
+                return Vector2.left;
             }
+            times.w++;
         }
         else if (Input.GetButton("Right"))
         {
-            times.y++;
             if (times.y > StepTreshold)
             {
-                isMoving = true;
                 spriteCon.Direction = spriteDirection.right;
-                pos += Vector2.right;
-                times.y = 0;
+                times = Vector4.zero;
+                return Vector2.right;
             }
+            times.y++;
         }
         else if (Input.GetButton("Up"))
         {
-            times.z++;
             if (times.z > StepTreshold)
             {
-                isMoving = true;
                 spriteCon.Direction = spriteDirection.up;
-                pos += Vector2.up;
-                times.z = 0;
+                times = Vector4.zero;
+                return Vector2.up;
             }
+            times.z++;
         }
         else if (Input.GetButton("Down"))
         {
-            times.x++;
             if (times.x > StepTreshold)
             {
-                isMoving = true;
                 spriteCon.Direction = spriteDirection.down;
-                pos += Vector2.down;
-                times.x = 0;
+                times = Vector4.zero;
+                return Vector2.down;
             }
+            times.x++;
         }
 
-        return pos;
+        return Vector2.zero;
     }
 
 }
